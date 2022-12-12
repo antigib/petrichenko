120f796fc200593f1d8a75a713df7f33aba77676
Antigib <veruovkin@mail.ru>
2022-12-12 19:45:23 +0300

    practice 1

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,22 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+console.log(numberOfFilms)
 
-// ++incr;
-// --decr;
-const vv = 57
-console.log(vv)
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(++incr);
-console.log(--decr);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(5%2);
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
 
-console.log(2 + 2 * 2 != 8);
-
-const isChecked = false,
-      isClose = false;
-
-console.log(isChecked || !isClose);
-
-
+console.log(personaMovieDB);
